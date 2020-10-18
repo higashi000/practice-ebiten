e9a76dd17aa48a6354e7aa728cc4afd78eb0cf8d
higashi000 <higashi.ict@gmail.com>
2020-10-18 21:11:31 +0900

    Add num up and down

--- main.go
+++ main.go
@@ -2,40 +2,33 @@ package main
 
 import (
 	"log"
+	"strconv"
 
 	"github.com/hajimehoshi/ebiten"
 	"github.com/hajimehoshi/ebiten/ebitenutil"
 )
 
 type Game struct {
-	H bool
-	J bool
-	K bool
-	L bool
+	Space bool
+    Height uint64
 }
 
 func (g *Game) Update(screen *ebiten.Image) error {
-	g.H = ebiten.IsKeyPressed(ebiten.KeyH)
-	g.J = ebiten.IsKeyPressed(ebiten.KeyJ)
-	g.K = ebiten.IsKeyPressed(ebiten.KeyK)
-	g.L = ebiten.IsKeyPressed(ebiten.KeyL)
+	g.Space = ebiten.IsKeyPressed(ebiten.KeySpace)
 
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	if g.H {
-		ebitenutil.DebugPrintAt(screen, "H", 0, 0)
+	if g.Space {
+        g.Height++
 	}
-	if g.J {
-		ebitenutil.DebugPrintAt(screen, "J", 0, 20)
-	}
-	if g.K {
-		ebitenutil.DebugPrintAt(screen, "K", 0, 40)
-	}
-	if g.L {
-		ebitenutil.DebugPrintAt(screen, "L", 0, 60)
-	}
+
+    if g.Height != 0 && !g.Space {
+        g.Height--
+    }
+
+    ebitenutil.DebugPrint(screen, strconv.FormatUint(g.Height, 10))
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
